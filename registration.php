90dd6868c7d89aa03a3053341f7abfe723fc7f1a
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-19 21:37:33 +0900

    Added form

--- registration.php
+++ registration.php
@@ -9,6 +9,36 @@
   <script src="main.js"></script>
 </head>
 <body>
-  
+  <div>
+    <?php 
+      if(isset($_POST['create'])) {
+        echo 'user submitted';
+      }
+    ?>
+  </div>
+  <div>
+    <form action="registration.php" method="POST">
+      <div class="container">
+        <h1>Registration</h1>
+        <p>Fill out the form.</p>
+        <label for="firstname"><b>First Name</b></label>
+        <input type="text" name="firstname" required>
+
+        <label for="lastname"><b>Last Name</b></label>
+        <input type="text" name="lastname" required>
+
+        <label for="email"><b>Email Address</b></label>
+        <input type="email" name="email" required>
+
+        <label for="phonenumber"><b>Phone No.</b></label>
+        <input type="text" name="phonenumber" required>
+
+        <label for="password"><b>Password</b></label>
+        <input type="password" name="password" required>
+
+        <input type="submit" name="create" value="signup">
+      </div>
+    </form>
+  </div>
 </body>
 </html>
