87730292d21bc720830a89ad846d0bb79608297e
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-19 21:55:33 +0900

    Added Submit function; and tested it  Completed bootstrap styling

--- registration.php
+++ registration.php
@@ -5,39 +5,51 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>User Registration</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="main.css">
-  <script src="main.js"></script>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+      rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
   <div>
     <?php 
       if(isset($_POST['create'])) {
-        echo 'user submitted';
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $phonenumber = $_POST['phonenumber'];
+        $password = $_POST['firstname'];
+
+        echo $firstname . " " . $lastname . " " . $email . " " .  $phonenumber . " " . $password;
       }
     ?>
+
   </div>
   <div>
     <form action="registration.php" method="POST">
       <div class="container">
-        <h1>Registration</h1>
-        <p>Fill out the form.</p>
-        <label for="firstname"><b>First Name</b></label>
-        <input type="text" name="firstname" required>
+        <div class="row">
+          <div class="col-sm-3 col-md-6 col-lg-8">
+            <h1>Registration</h1>
+            <p>Fill out the form.</p>
+            <hr class="mb-3">
+            <label for="firstname"><b>First Name</b></label>
+            <input class="form-control" type="text" name="firstname" required>
 
-        <label for="lastname"><b>Last Name</b></label>
-        <input type="text" name="lastname" required>
+            <label for="lastname"><b>Last Name</b></label>
+            <input class="form-control" type="text" name="lastname" required>
 
-        <label for="email"><b>Email Address</b></label>
-        <input type="email" name="email" required>
+            <label for="email"><b>Email Address</b></label>
+            <input class="form-control" type="email" name="email" required>
 
-        <label for="phonenumber"><b>Phone No.</b></label>
-        <input type="text" name="phonenumber" required>
+            <label for="phonenumber"><b>Phone No.</b></label>
+            <input class="form-control" type="text" name="phonenumber" required>
 
-        <label for="password"><b>Password</b></label>
-        <input type="password" name="password" required>
-
-        <input type="submit" name="create" value="signup">
-      </div>
+            <label for="password"><b>Password</b></label>
+            <input class="form-control" type="password" name="password" required>
+            <hr class="mb-3">
+            <input class="btn btn-primary" type="submit" name="create" value="signup">
+          </div>   <!-- col-sm-3  -->
+        </div>    <!-- row -->
+      </div>     <!-- container -->
     </form>
   </div>
 </body>
